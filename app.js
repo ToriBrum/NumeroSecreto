9a06eb80d369de20f5657b7636bc676929356c9a
Victória Brum <victoria.brum.14@gmail.com>
2024-12-12 19:21:37 -0300

    Criação de um gerador aleatório de números utilizando Math.random.

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 42;
+
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
+console.log(numeroSecreto);
+
 let chuteUsuario;
 let tentativas = 1;
 
 while (chuteUsuario != numeroSecreto){
     chuteUsuario = prompt('Escolha um número entre 1 e 100');
     if (chuteUsuario == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto! É ${numeroSecreto}. Você fez ${tentativas} tentativas.`);
+        break;
     } else {
         if (chuteUsuario > numeroSecreto){
-            alert(`Quase acertou, é menor que ${chuteUsuario}`);
+            alert(`É menor que ${chuteUsuario}`);
         } else{
-            alert(`Quase acertou, é maior que ${chuteUsuario}`);
+            alert(`É maior que ${chuteUsuario}`);
         }
         tentativas++
     }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto! É ${numeroSecreto}. Você fez ${tentativas} ${palavraTentativa}`);
